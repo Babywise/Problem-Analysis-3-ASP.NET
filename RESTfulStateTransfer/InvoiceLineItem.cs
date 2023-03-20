9050a3249e7afb167a7b0cb6b04572c16b5dd24b
Babywise <islamdoesart@gmail.com>
2023-03-19 21:07:03 -0400

    started

---
+++ RESTfulStateTransfer/InvoiceLineItem.cs
@@ -0,0 +1,16 @@
+﻿namespace VendorInvoicing.Entities
+{
+    public class InvoiceLineItem
+    {
+        public int InvoiceLineItemId { get; set; }
+
+        public double? Amount { get; set; }
+
+        public string? Description { get; set; }
+
+        // FK:
+        public int? InvoiceId { get; set; }
+
+        // Nav prop to invoice:
+    }
+}
---
+++ RESTfulStateTransfer/PaymentTerms.cs
@@ -0,0 +1,13 @@
+﻿namespace VendorInvoicing.Entities
+{
+    public class PaymentTerms
+    {
+        public int PaymentTermsId { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public int DueDays { get; set; }
+
+        // Nav to invoices:
+    }
+}
